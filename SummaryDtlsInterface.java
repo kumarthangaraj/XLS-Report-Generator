54c0e2c34a9181cb8370bfe657276692a217158b
kumarthangaraj <kumar.thangaraj@gmail.com>
2015-09-22 14:54:47 +0530

    Create SummaryDtlsInterface.java
    Interface to access summary object

---
+++ SummaryDtlsInterface.java
@@ -0,0 +1,9 @@
+package com.krkh.xls.util;
+
+import java.util.List;
+
+public interface SummaryDtlsInterface {
+	public List getSummaryLL();
+	public void setSummaryLL(List list);
+	public String getQuery();
+}
